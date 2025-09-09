05fcd2d210168be14340f41e7f037d5e647b7b3a
boongxs <davis.pilny@gmail.com>
2025-09-09 13:33:08 +0200

    Added some light user notification elements for the video file playback to improve UX

--- stepstones/Views/EnlargeVideoView.xaml.cs
+++ stepstones/Views/EnlargeVideoView.xaml.cs
@@ -1,18 +1,30 @@
 ﻿using System.IO;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Threading;
+using System.Windows.Media.Animation;
 using stepstones.ViewModels;
+using Vlc.DotNet.Core;
 
 namespace stepstones.Views
 {
     public partial class EnlargeVideoView : UserControl
     {
+        private readonly DispatcherTimer _indicatorTimer;
+        private bool _videoHasEnded = false;
+
         public EnlargeVideoView()
         {
             InitializeComponent();
 
             this.DataContextChanged += EnlargeVideoView_DataContextChanged;
             this.Unloaded += EnlargeVideoView_Unloaded;
+
+            _indicatorTimer = new DispatcherTimer
+            {
+                Interval = TimeSpan.FromMilliseconds(750)
+            };
+            _indicatorTimer.Tick += IndicatorTimer_Tick;
 
             var currentAssembly = System.Reflection.Assembly.GetEntryAssembly();
             if (currentAssembly == null)
@@ -39,6 +51,8 @@ namespace stepstones.Views
             };
 
             MediaPlayer.SourceProvider.CreatePlayer(vlcLibDirectory, options);
+
+            MediaPlayer.SourceProvider.MediaPlayer.EndReached += MediaPlayer_EndReached;
         }
 
         private void EnlargeVideoView_DataContextChanged(object sender, DependencyPropertyChangedEventArgs e)
@@ -57,12 +71,71 @@ namespace stepstones.Views
             MediaPlayer.Dispose();
         }
 
+        private void MediaPlayer_EndReached(object sender, VlcMediaPlayerEndReachedEventArgs e)
+        {
+            Dispatcher.Invoke(() =>
+            {
+                _videoHasEnded = true;
+
+                StopAnimationAndHide(PlayIcon);
+                StopAnimationAndHide(PauseIcon);
+
+                EndOverlay.Opacity = 1;
+            });
+        }
+
         private void MediaPlayer_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
         {
+            if (_videoHasEnded)
+            {
+                e.Handled = true;
+                return;
+            }
+
+            if (MediaPlayer.SourceProvider.MediaPlayer.IsPlaying())
+            {
+                StopAnimationAndHide(PlayIcon);
+                AnimateIcon(PauseIcon, "FadeInAnimation");
+            }
+            else
+            {
+                StopAnimationAndHide(PauseIcon);
+                AnimateIcon(PlayIcon, "FadeInAnimation");
+            }
+
+            _indicatorTimer.Stop();
+            _indicatorTimer.Start();
+
             MediaPlayer.SourceProvider.MediaPlayer.Pause();
 
             // prevent click event from bubbling up and potentially closing the dialog
             e.Handled = true;
+        }
+
+        private void IndicatorTimer_Tick(object sender, EventArgs e)
+        {
+            if (PlayIcon.Opacity > 0)
+            {
+                AnimateIcon(PlayIcon, "FadeOutAnimation");
+            }
+            if (PauseIcon.Opacity > 0)
+            {
+                AnimateIcon(PauseIcon, "FadeOutAnimation");
+            }
+
+            _indicatorTimer.Stop();
+        }
+
+        private void AnimateIcon(FrameworkElement icon, string storyboardName)
+        {
+            var storyboard = (Storyboard)this.Resources[storyboardName];
+            storyboard.Begin(icon);
+        }
+
+        private void StopAnimationAndHide(FrameworkElement icon)
+        {
+            icon.BeginAnimation(UIElement.OpacityProperty, null);
+            icon.Opacity = 0;
         }
     }
 }
