8b94cb27b3d0a9e0fad27a583eedcf49fce152ca
boongxs <davis.pilny@gmail.com>
2025-09-12 16:24:39 +0200

    Minor visual changes

--- stepstones/Services/Core/IFileService.cs
+++ stepstones/Services/Core/IFileService.cs
@@ -4,7 +4,7 @@ namespace stepstones.Services.Core
 {
     public interface IFileService
     {
-        Task<Dictionary<string, string?>> CopyFilesAsync(IEnumerable<string> sourceFilePaths, string destinationFolderPath);
+        Task<Dictionary<string, string>> CopyFilesAsync(IEnumerable<string> sourceFilePaths, string destinationFolderPath);
         IEnumerable<string> GetAllFiles(string folderPath);
         void DeleteMediaFile(MediaItem item);
     }
