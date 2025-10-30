60084854486a47cdf34262d96724c42594b35e0d
boongxs <davis.pilny@gmail.com>
2025-10-30 16:13:03 +0100

    Updated icons in Video and Audio files Enlarge views

---
+++ stepstones/Converters/VolumeToIconStateConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace stepstones.Converters
+{
+    public class VolumeToIconStateConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is double volume)
+            {
+                // if below 0.5, return low, otherwise return high
+                return (volume < 0.5) ? "Low" : "High";
+            }
+
+            return "High"; // default to high if something goes wrong
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
